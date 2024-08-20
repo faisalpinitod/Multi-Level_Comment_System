b6d69581ec9e72994c3bda9e01b1f362535d9d76
faisalpinitod <faisalpinitod@gmail.com>
2024-08-20 11:17:55 +0530

    The user.model , db.js and server.js created and tested with .env file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to the database
+connectDB();
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
